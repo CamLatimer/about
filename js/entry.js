d8bf4f7b1769189da5b4578c6446c946b075ed45
camlatimer <camlatimer@gmail.com>
2017-09-16 04:01:52 -0400

    adds nav and event handling for mobile

--- js/entry.js
+++ js/entry.js
@@ -4,24 +4,41 @@ window.addEventListener('load', function(){
   let demos = document.querySelectorAll('.content__text');
   let demoImgs = document.querySelectorAll('img');
   let headers = document.querySelectorAll('.demo__header');
-  navItems.forEach(function(item, index){
+  let navArrows = document.querySelectorAll('nav h1');
+  let index = 0;
 
-
+  navItems.forEach(function(item, i){
     item.addEventListener('click', function(){
-      document.querySelector('.highlight').classList.remove('highlight');
-      document.querySelector('.showText').classList.remove('showText');
-      document.querySelector('.showHeader').classList.remove('showHeader');
-      let img = document.querySelector('.showImg');
-      if(img) img.classList.remove('showImg');
-
-      demos[index].classList.add('showText');
-      demoImgs[index].classList.add('showImg');
-      headers[index].classList.add('showHeader');
-      item.classList.add('highlight');
-
+      index = i;
+      showDemo(index, item);
     })
-
-
-
   })
+
+  navArrows.forEach(function(item, i){
+    item.addEventListener('click', function(){
+      if(item.classList.contains('forward')){
+        index = index === 4 ? 0 : index + 1;
+      } else {
+        index = index === 0 ? 4 : index - 1;
+      }
+      showDemo(index);
+    })
+  })
+
+  function showDemo(demoIndex, item){
+    console.log(demoIndex);
+    if(item){
+      document.querySelector('.highlight').classList.remove('highlight');
+      item.classList.add('highlight');
+    }
+    document.querySelector('.showText').classList.remove('showText');
+    document.querySelector('.showHeader').classList.remove('showHeader');
+    let img = document.querySelector('.showImg');
+    if(img) img.classList.remove('showImg');
+    demos[demoIndex].classList.add('showText');
+    demoImgs[demoIndex].classList.add('showImg');
+    headers[demoIndex].classList.add('showHeader');
+    console.log(demoIndex);
+  }
+
 })
