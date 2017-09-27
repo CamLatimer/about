079db8a8e0307ecc31f348bac401afc1ebb8cd66
camlatimer <camlatimer@gmail.com>
2017-09-26 21:58:05 -0400

    tweaks nav on mobile, tweaks js to make sure project name in nav is highlighted as screen is resized

--- js/entry.js
+++ js/entry.js
@@ -1,34 +1,37 @@
 window.addEventListener('load', function(){
+
   let navItems = document.querySelectorAll('.nav__list li');
   let demos = document.querySelectorAll('.content__text');
   let demoImgs = document.querySelectorAll('img');
   let headers = document.querySelectorAll('.demo__header');
-  let navArrows = document.querySelectorAll('nav h1');
+  let navArrows = document.querySelectorAll('.navToggles');
   let index = 0;
 
+  // set index to element's index # when clicked then show/hide elements
   navItems.forEach(function(item, i){
     item.addEventListener('click', function(){
       index = i;
-      showDemo(index, item);
+      showDemo(index);
     })
   })
 
+// change index with nav arrows
   navArrows.forEach(function(item, i){
     item.addEventListener('click', function(){
       if(item.classList.contains('forward')){
-        index = index === 4 ? 0 : index + 1;
+        index = index === 3 ? 0 : index + 1;
       } else {
-        index = index === 0 ? 4 : index - 1;
+        index = index === 0 ? 3 : index - 1;
       }
       showDemo(index);
     })
   })
 
-  function showDemo(demoIndex, item){
-    if(item){
-      document.querySelector('.highlight').classList.remove('highlight');
-      item.classList.add('highlight');
-    }
+  // highlight selected project in nav menu and show-hide header and images
+  function showDemo(demoIndex){
+    document.querySelector('.highlight').classList.remove('highlight');
+    navItems[demoIndex].classList.add('highlight');
+
     document.querySelector('.showText').classList.remove('showText');
     document.querySelector('.showHeader').classList.remove('showHeader');
     let img = document.querySelector('.showImg');
